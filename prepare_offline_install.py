c7b69182f27e873c1b722b3b29d6227631a280da
xorpd <xorpd@xorpd.net>
2016-11-02 20:43:49 +0200

    Making sure install_assets dir exists.

--- prepare_offline_install.py
+++ prepare_offline_install.py
@@ -10,9 +10,14 @@ current_path = os.path.dirname(os.path.abspath(__file__))
 def run():
     print('Collecting assets...')
 
+    # Make sure that install_assets directory exists:
+    install_assets_dir = os.path.join(current_path,'install_assets')
+    if not os.path.exists(install_assets_dir):
+        os.makedirs(install_assets_dir)
     # ez_setup.py, for setuptools. This could be useful for later installation
     # of the packages using python setup.py install
-    ez_setup_path = os.path.join(current_path,'install_assets','ez_setup.py')
+
+    ez_setup_path = os.path.join(install_assets_dir,'ez_setup.py')
     if not os.path.isfile(ez_setup_path):
         # Download if non existent:
         urllib.urlretrieve(EZ_SETUP_URL,ez_setup_path)
