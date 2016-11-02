b2973cf5da373bb44fa70f6157be4d7759760d5e
xorpd <xorpd@xorpd.net>
2016-11-02 16:05:42 +0200

    Removed unused imports from __init__.py file.

--- idsearch/__init__.py
+++
@@ -1,8 +0,0 @@
-from idsearch import idb_util
-from idsearch import searcher
-
-from idsearch.searcher import *
-from idsearch.types import *
-
-sdb_path= idb_util.gen_sdb_path(idaapi.cvar.database_idb)
-sdb = searcher.load_sdb(sdb_path)
