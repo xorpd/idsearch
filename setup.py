2e6fb56e4798f1a9d816c5c75d9134cd3aec616a
xorpd <xorpd@xorpd.net>
2016-11-02 19:27:30 +0200

    Added setup.py file for package installation.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='idsearch',
+    version='0.1',
+    description='Fast searcher for the Interactive Disassembler',
+    url='https://github.com/xorpd/idsearch',
+    author='xorpd',
+    author_email='xorpd@xorpd.net',
+    license='GPLv3',
+    packages=['idsearch'],
+    zip_safe=False)
